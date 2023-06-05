0c68d046fd41d6ef2e41db9a76b01caba4c46e0c
Khurram Mustafa <khurram.mustafa@semsee.com>
2023-06-05 17:30:01 -0400

    second commit

--- sample.exs
+++ sample.exs
@@ -4,8 +4,16 @@ defmodule Mine do
   def yell(text) do
     IO.puts(String.upcase(text))
   end
+
+  def multi(mystery \\ nil) do
+    case mystery do
+      nil -> "Wow you entered nothing!"
+      1 -> "Youre one of a kind!"
+      2 -> "Two heads are better than one."
+      3 -> "Third times the charm!"
+      _ -> "Please stop."
+    end
+  end
 end
 
-text = "Hi my name is Mustafa!"
-
-Mine.yell(text)
+IO.puts(Mine.multi())
