2dbe19496d89ec8e189741e71a9cfdb35690bc05
KhromeM <68210337+KhromeM@users.noreply.github.com>
2023-06-06 21:19:01 -0400

    more readable

--- exercises.exs
+++ exercises.exs
@@ -1,43 +1,36 @@
 
-
-
-# def large_lines!(path) do
-#   File.stream!(path) #returns a stream
-#   |> Stream.map(&(String.trim_trailing(&1,"\n")))
-#   |> Enum.filter(&(String.length(&1) > 80))
-# end
-
 defmodule LLines do
 
-  #A lines_lengths!/1 that takes a file path and returns a list of numbers, with each number representing the length of the corresponding line from the file.
+  #1 lines_lengths!/1 that takes a file path and returns a list of numbers, with each number representing the length of the corresponding line from the file.
   def lines_length!(path) do
     File.stream!(path)
-    |> Stream.map(&(String.trim_trailing(&1,"\n"))) #Remove the newline chars
+    |> Stream.map(&(String.trim_trailing(&1,"\n")))
     |> Enum.map(&String.length/1)
   end
 
-  #A longest_line_length!/1 that returns the length of the longest line in a file.
+  #2 longest_line_length!/1 that returns the length of the longest line in a file.
   def longest_line_length!(path) do
     File.stream!(path)
-    |> Stream.map(&(String.trim_trailing(&1,"\n")))
-    |> Stream.map(&String.length/1)
-    |> Enum.reduce(&(max(&1,&2)))
+    |> Stream.map(&(String.trim_trailing(&1,"\n")) |> String.length())
+    |> Enum.max()
+    # |> Enum.reduce(&(max(&1,&2)))
+
   end
 
-  #A longest_line!/1 that returns the contents of the longest line in a file.
+  #3 longest_line!/1 that returns the contents of the longest line in a file.
   def longest_line!(path) do
     length = longest_line_length!(path)
-    res = File.stream!(path)
+    File.stream!(path)
     |> Stream.map(&(String.trim_trailing(&1,"\n")))
     |> Enum.filter(&(String.length(&1) == length))
-    hd(res)
+    |> hd()
   end
 
-  #A words_per_line!/1 that returns a list of numbers, with each number representing the word count in a file.
+  #4 words_per_line!/1 that returns a list of numbers, with each number representing the word count in a file.
   def words_per_line(path) do
     File.stream!(path)
     |> Stream.map(&(String.trim_trailing(&1,"\n")))
-    |> Enum.map(&(length(String.split(&1))))
+    |> Enum.map(&(String.split(&1)) |> length())
   end
 
 end
